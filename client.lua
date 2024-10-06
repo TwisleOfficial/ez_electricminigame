559eaa4800522f0af170f99eeebf6de139696696
Rayaan Uddin <rayaan.uddin@outlook.com>
2024-10-06 19:25:48 +0100

    Created Script

---
+++ client.lua
@@ -0,0 +1,34 @@
+local wiringfix
+
+RegisterNuiCallback('wiringfixSuccess', function(_, cb)
+    if not wiringfix then return cb('ok') end
+    SetNuiFocus(false, false)
+    wiringfix:resolve(true)
+    wiringfix = nil
+    cb('ok')
+end)
+
+RegisterNuiCallback('wiringfixFail', function(_, cb)
+    if not wiringfix then return cb('ok') end
+    SetNuiFocus(false, false)
+    wiringfix:resolve(false)
+    wiringfix = nil
+    cb('ok')
+end)
+
+local function WiringFix(time)
+    wiringfix = promise.new()
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = 'startWiring',
+        time = time
+    })
+    return Citizen.Await(wiringfix)
+end
+exports('WiringFix', WiringFix)
+
+-- Test Command
+RegisterCommand("testwiringfix",function()
+    local success = exports["ez_wiringfix"]:WiringFix(30)
+    print(success)
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Rayaan Uddin'
+description 'Among us electric minigame'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/*.css',
+    'html/*.js',
+    'html/sounds/*.mp3',
+}
